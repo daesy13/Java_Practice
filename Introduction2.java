246e5aeaa1a4b46a1e32cc98c65ea75af6c6ec43
daesy <daesyramos@hotmail.com>
2020-03-16 11:48:23 -0700

    initial

---
+++ Introduction2.java
@@ -0,0 +1,9 @@
+import java.io.Console;
+
+public class Introduction2{
+    public static void main(String[] args) {
+        Console console = System.console();
+        console.printf("Hello, my name is Daesy\n");
+        console.printf("Daesy is learning how to write Java.");
+    }
+}
